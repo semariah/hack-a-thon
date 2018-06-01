24b7b54ba2afd3f8bbd622d680d2507d1a59bd29
sk <semaria.eyzac@gmail.com>
2018-06-01 10:56:24 -0700

    add the new POST route in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import models.Team;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -15,6 +16,15 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String description = request.queryParams("description");
+            String memberToAdd = request.queryParams("memberToAdd");
+            Team newTeam = new Team(name, description, memberToAdd);
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
