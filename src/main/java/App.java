dc44701f53772e1aa26e5e0cbb1541913a866eb1
sk <semaria.eyzac@gmail.com>
2018-06-01 10:40:44 -0700

    create an index.hbs fileand import the Map

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 
 import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -9,5 +11,10 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
