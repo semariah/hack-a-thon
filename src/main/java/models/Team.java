c0c68e298c96a6917d7046dbaee88e16e65f7b7e
sk <semaria.eyzac@gmail.com>
2018-06-01 09:36:40 -0700

    add a test to check if the object Team correctly instantiates but still fails because of the array list for members

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,17 @@
 package models;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+    private String name;
+    private String description;
+    private static List<String> members = new ArrayList<String>();
+
+    public Team(String name, String description, ArrayList<String> membersToAdd){
+        this.name = name;
+        this.description = description;
+        this.members.addAll(membersToAdd);
+    }
 }
---
+++ src/test/java/models/TeamTest.java
@@ -0,0 +1,28 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class TeamTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewTeamObjectGetsCorrectlyCreated_true() throws Exception {
+        Team team = new Team("Team: JS", "JS students of epicodus", ("bella", "sem");
+        assertEquals(true, team instanceof Team);
+    }
+
+
+}
