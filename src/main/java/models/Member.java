c4328c03d7419d5a5f10ed43a91a977f5b6afb66
sk <semaria.eyzac@gmail.com>
2018-06-08 12:35:19 -0700

    create a member.java data model with all the necessary properties and the constructor and the getters & setters and the equals & hashcods

---
+++ src/main/java/models/Member.java
@@ -0,0 +1,53 @@
+package models;
+
+import java.util.Objects;
+
+public class Member {
+    private String name;
+    private int id;
+    private int teamId;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return id == member.id &&
+                teamId == member.teamId &&
+                Objects.equals(name, member.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, id, teamId);
+    }
+}
