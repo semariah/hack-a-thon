41c7b17837b0fb31adf5008430f17abda8b78917
sk <semaria.eyzac@gmail.com>
2018-06-08 11:03:39 -0700

    pass a test that sets id to teams

--- src/test/java/Dao/Sql2oTeamDaoTest.java
+++ src/test/java/Dao/Sql2oTeamDaoTest.java
@@ -33,7 +33,7 @@ public class Sql2oTeamDaoTest {
         Team team = setupNewTeam();
         int originalTeamId = team.getId();
         teamDao.add(team);
-        assertEquals(originalTeamId, team.getId());
+        assertNotEquals(originalTeamId, team.getId());
     }
 
     @Test
